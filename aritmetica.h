5e01f53fb98c9b14e5e9de9936f48f4ffed6ca65
Rodrigo <rodrigogg@gmail.com>
2026-02-15 16:47:35 -0600

    Se agregó multiplicar

--- aritmetica.h
+++ aritmetica.h
@@ -4,6 +4,11 @@ float sumar(float a, float b){
 }
 
 
-float resta (float a, float b){
+float restar(float a, float b){
  return a - b;
+}
+
+
+float multiplicar(float a, float b){
+ return a * b;
 }
